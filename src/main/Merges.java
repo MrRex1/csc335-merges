d74112c792fc23fbe26781651dab558a2779d827
Ferrari-559 <ad_o_n@hotmail.com>
2022-03-31 18:30:20 -0700

    F

--- src/main/Merges.java
+++ src/main/Merges.java
@@ -3,6 +3,6 @@ package main;
 public class Merges {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from the master branch");
+		System.out.println("Hell");
 	}
 }//end_class
